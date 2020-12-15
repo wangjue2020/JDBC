b0e7eacfce41b7eed59fdddd5728a5a05f7e05fd
wangjue <wangjue60@yahoo.com>
2020-12-15 16:38:04 -0500

    format some files and getColumnLabel VS getColumnName

--- JDBC/src/com/prepared/statement/crud/QueryForCustomer.java
+++ JDBC/src/com/prepared/statement/crud/QueryForCustomer.java
@@ -23,30 +23,30 @@ public class QueryForCustomer {
 	/**
 	 * @Description 针对customers表的通用的查询操作
 	 */
-	public List<Customer> queryForCustomers(String sql, Object ...args){
+	public List<Customer> queryForCustomers(String sql, Object... args) {
 		ArrayList<Customer> res = new ArrayList<>();
 		Connection connection = null;
 		PreparedStatement prepareStatement = null;
 		ResultSet resultSet = null;
-		try{
+		try {
 			connection = JDBCutils.getConnection();
 			prepareStatement = connection.prepareStatement(sql);
-			for(int i = 1; i <= args.length; i++){
-				prepareStatement.setObject(i, args[i-1]);
+			for (int i = 1; i <= args.length; i++) {
+				prepareStatement.setObject(i, args[i - 1]);
 			}
 			resultSet = prepareStatement.executeQuery();
-			//获取结果集的元数据
+			// 获取结果集的元数据
 			ResultSetMetaData metaData = resultSet.getMetaData();
-			//通过ResultSetMetaData获取结果集中的列数
+			// 通过ResultSetMetaData获取结果集中的列数
 			int numColumn = metaData.getColumnCount();
-			while(resultSet.next()){
+			while (resultSet.next()) {
 				Customer c = new Customer();
-				for(int i=0; i < numColumn; i++){
-					//获取列值
-					Object columnValue = resultSet.getObject(i+1);
-					//获取每个列的列名
-					String columnName = metaData.getColumnName(i+1);
-					//给customer 对象指定的columnName属性，赋值为columnValue，通过反射
+				for (int i = 0; i < numColumn; i++) {
+					// 获取列值
+					Object columnValue = resultSet.getObject(i + 1);
+					// 获取每个列的列名
+					String columnName = metaData.getColumnName(i + 1);
+					// 给customer 对象指定的columnName属性，赋值为columnValue，通过反射
 					Field field = Customer.class.getDeclaredField(columnName);
 					field.setAccessible(true);
 					field.set(c, columnValue);
@@ -54,45 +54,47 @@ public class QueryForCustomer {
 
 				res.add(c);
 			}
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			JDBCutils.closeResource(connection, prepareStatement, resultSet);
 		}
 		return res;
 	}
+
 	@Test
-	public void testQueryForCustomers(){
-		
-		List<Customer> res = queryForCustomers("select id, name from customers where id >=?;",1 );
+	public void testQueryForCustomers() {
+
+		List<Customer> res = queryForCustomers("select id, name from customers where id >=?;", 1);
 		System.out.println(res);
 	}
+
 	@Test
 	public void test() {
 		Connection connection = null;
 		PreparedStatement prepareStatement = null;
 		ResultSet resultset = null;
-		try{
+		try {
 			connection = JDBCutils.getConnection();
 			String sql = "select id, name, email, birth from customers where id = ?;";
 			prepareStatement = connection.prepareStatement(sql);
 			prepareStatement.setObject(1, 1);
-			//执行,并返回结果集
+			// 执行,并返回结果集
 			resultset = prepareStatement.executeQuery();
-			//处理结果集
-			//next():	判断结果集的下一条是否有数据，如果有数据返回true，并指针下移；如果返回false，指针不会下移
-			if(resultset.next()){
-				//获取当前这条数据的各个字段值
+			// 处理结果集
+			// next(): 判断结果集的下一条是否有数据，如果有数据返回true，并指针下移；如果返回false，指针不会下移
+			if (resultset.next()) {
+				// 获取当前这条数据的各个字段值
 				int id = resultset.getInt(1);
 				String name = resultset.getString(2);
 				String email = resultset.getString(3);
 				Date birthday = resultset.getDate(4);
-				
+
 				Customer customer = new Customer(id, name, email, birthday);
 				System.out.println(customer);
-				
+
 			}
-		}catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			JDBCutils.closeResource(connection, prepareStatement, resultset);
--- JDBC/src/com/prepared/statement/crud/QueryForOrder.java
+++ JDBC/src/com/prepared/statement/crud/QueryForOrder.java
@@ -15,17 +15,29 @@ import com.bean.Order;
 import com.util.JDBCutils;
 
 /**
- * @Description	针对于Order 表的通用的查询操作
+ * @Description 针对于Order 表的通用的查询操作
  * @author Wangjue
  *
  */
 public class QueryForOrder {
 	@Test
-	public void queryForOrderTest(){
-		List<Order> orders = queryForOrder("select * from `order`;");
+	public void queryForOrderTest() {
+		List<Order> orders = queryForOrder1("select * from `order`;");
 		System.out.println(orders);
+		List<Order> orders1 = queryForOrder(
+				"select order_id orderId, order_name orderName, order_date orderDate from `order`;");
+		System.out.println(orders);
+
 	}
-	public List<Order> queryForOrder(String sql, Object...args){
+	/*
+	 * 针对于表的字段名与类的属性名不相同的情况：
+	 * 1、必须声明sql时，使用类的属性名来命名字段的别名
+	 * 2、使用ResultSetMetaData时，需要使用getColmnLabel()来替换getColumnName(),
+	 * 		获取列的别名。
+	 * 说明：如果sql中没有给字段起别名，getColumnLabel() 获取的就是列名
+	 */
+
+	public List<Order> queryForOrder(String sql, Object... args) {
 		List<Order> res = new ArrayList<>();
 		Connection connection = null;
 		PreparedStatement ps = null;
@@ -33,19 +45,19 @@ public class QueryForOrder {
 		try {
 			connection = JDBCutils.getConnection();
 			ps = connection.prepareStatement(sql);
-			for(int i=0; i < args.length; i++){
-				ps.setObject(i+1,args[i]);
+			for (int i = 0; i < args.length; i++) {
+				ps.setObject(i + 1, args[i]);
 			}
 			rs = ps.executeQuery();
 			ResultSetMetaData metaData = rs.getMetaData();
 			int numColumn = metaData.getColumnCount();
-			while (rs.next()){
+			while (rs.next()) {
 				Order o = new Order();
-				for(int i = 0; i<numColumn; i++){
-					Object columnValue = rs.getObject(i+1);
-					String columnName = metaData.getColumnName(i+1);
-					String[] splitedName = columnName.split("_");
-					columnName = splitedName[0]+splitedName[1].substring(0, 1).toUpperCase()+splitedName[1].substring(1);
+				for (int i = 0; i < numColumn; i++) {
+					Object columnValue = rs.getObject(i + 1);
+					// metaData.getColumnName()：获取列名
+					// metaData.getColumnLabel()：获取别名
+					String columnName = metaData.getColumnLabel(i + 1);
 					Field field = Order.class.getDeclaredField(columnName);
 					field.setAccessible(true);
 					field.set(o, columnValue);
@@ -55,34 +67,73 @@ public class QueryForOrder {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally{
+		} finally {
 			JDBCutils.closeResource(connection, ps, rs);
 		}
 		return res;
 	}
+
+	@Deprecated
+	public List<Order> queryForOrder1(String sql, Object... args) {
+		List<Order> res = new ArrayList<>();
+		Connection connection = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			connection = JDBCutils.getConnection();
+			ps = connection.prepareStatement(sql);
+			for (int i = 0; i < args.length; i++) {
+				ps.setObject(i + 1, args[i]);
+			}
+			rs = ps.executeQuery();
+			ResultSetMetaData metaData = rs.getMetaData();
+			int numColumn = metaData.getColumnCount();
+			while (rs.next()) {
+				Order o = new Order();
+				for (int i = 0; i < numColumn; i++) {
+					Object columnValue = rs.getObject(i + 1);
+					String columnName = metaData.getColumnName(i + 1);
+					String[] splitedName = columnName.split("_");
+					columnName = splitedName[0] + splitedName[1].substring(0, 1).toUpperCase()
+							+ splitedName[1].substring(1);
+					Field field = Order.class.getDeclaredField(columnName);
+					field.setAccessible(true);
+					field.set(o, columnValue);
+				}
+				res.add(o);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JDBCutils.closeResource(connection, ps, rs);
+		}
+		return res;
+	}
+
 	@Test
-	public void test(){
+	public void test() {
 		Connection connection = null;
 		PreparedStatement preparestatement = null;
-		ResultSet rs =null;
-		try{
-			connection  = JDBCutils.getConnection();
+		ResultSet rs = null;
+		try {
+			connection = JDBCutils.getConnection();
 			String sql = "select order_id, order_name, order_date from `order` where order_id=?;";
 			preparestatement = connection.prepareStatement(sql);
 			preparestatement.setObject(1, 1);
 			rs = preparestatement.executeQuery();
-			if(rs.next()){
+			if (rs.next()) {
 				int id = (int) rs.getObject(1);
 				String name = (String) rs.getObject(2);
 				Date date = (Date) rs.getObject(3);
-				
-				Order order = new Order(id,name,date);
+
+				Order order = new Order(id, name, date);
 				System.out.println(order);
 			}
-			
-		} catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		} finally{
+		} finally {
 			JDBCutils.closeResource(connection, preparestatement, rs);
 		}
 	}
