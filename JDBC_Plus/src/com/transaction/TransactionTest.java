b1caa5eeb02f064bbc5fe7827b7b2d9605186ab9
wangjue <wangjue60@yahoo.com>
2020-12-19 15:01:52 -0500

    transaction isolation

--- JDBC_Plus/src/com/transaction/TransactionTest.java
+++ JDBC_Plus/src/com/transaction/TransactionTest.java
@@ -1,11 +1,17 @@
 package com.transaction;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
+import com.bean.User;
 import com.utils.JDBCutils;
 
 /*
@@ -131,5 +137,64 @@ public class TransactionTest {
 		}
 		return 0;
 	}
-
+	
+	@Test
+	public void testTransactionSelect() throws Exception{
+		Connection connection  = JDBCutils.getConnection();
+		connection.setTransactionIsolation(1);
+		//获取当前连接的隔离级别
+		System.out.println(connection.getTransactionIsolation());
+		//设置数据库的隔离级别
+		connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+		connection.setAutoCommit(false);
+		String sql = "select user, password, balance from user_table where user = ?";
+		List<User> instance = getInstance(connection, User.class, sql, "CC");
+		System.out.println(instance);
+	}
+	@Test
+	public void testTransactionUpdate() throws Exception{
+		Connection connection  = JDBCutils.getConnection();
+		String sql = "update user_table set balance =? where user=?";
+		generalUpdate1(connection, sql,5000,"CC");
+		
+		Thread.sleep(15000);
+		System.out.println("修改结束");
+		
+	}
+	//通用的查询操作，用于返回数据表中的一条记录（Version2.0 考虑上事务）
+	public <T> List<T> getInstance(Connection connection, Class<T> clazz,String sql, Object...args){
+		List<T> res = new ArrayList<>();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = connection.prepareStatement(sql);
+			for (int i = 0; i < args.length; i++) {
+				ps.setObject(i + 1, args[i]);
+			}
+			rs = ps.executeQuery();
+			ResultSetMetaData metaData = rs.getMetaData();
+			int numColumn = metaData.getColumnCount();
+			while (rs.next()) {
+				T newInstance = clazz.newInstance();
+//				Order o = new Order();
+				for (int i = 0; i < numColumn; i++) {
+					Object columnValue = rs.getObject(i + 1);
+					// metaData.getColumnName()：获取列名
+					// metaData.getColumnLabel()：获取别名
+					String columnName = metaData.getColumnLabel(i + 1);
+					Field field = clazz.getDeclaredField(columnName);
+//					Field field = Order.class.getDeclaredField(columnName);
+					field.setAccessible(true);
+					field.set(newInstance, columnValue);
+				}
+				res.add(newInstance);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JDBCutils.closeResource(null, ps, rs);
+		}
+		return res;
+	}
 }
