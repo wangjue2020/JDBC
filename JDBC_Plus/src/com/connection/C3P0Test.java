4cbb931f5cd3f5b76b604ca3f17ce0d4e6e1dcc5
wangjue <wangjue60@yahoo.com>
2020-12-19 16:57:10 -0500

    c3p0 database connection pool

--- JDBC_Plus/src/com/connection/C3P0Test.java
+++ JDBC_Plus/src/com/connection/C3P0Test.java
@@ -1,5 +1,29 @@
 package com.connection;
 
-public class C3P0Test {
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
 
+import org.junit.Test;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class C3P0Test {
+	//方式一
+	@Test
+	public void testGetConnection() throws Exception{
+		//获c3p0数据库连接池
+		ComboPooledDataSource cpds = new ComboPooledDataSource();
+		cpds.setDriverClass( "com.mysql.jdbc.Driver" ); //loads the jdbc driver            
+		cpds.setJdbcUrl( "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" );
+		cpds.setUser("root");                                  
+		cpds.setPassword("admin");  
+		//通过设置相关的参数，对数据库连接池进行管理
+		//设置初始时数据库连接池中的连接数
+		cpds.setInitialPoolSize(10);
+		
+		Connection connection = cpds.getConnection();
+		System.out.println(connection);
+	}
+	//方式二： 使用配置文件
+	
 }
